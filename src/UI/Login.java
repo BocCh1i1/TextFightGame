e47d8e1bee9dd8482bc3b1a97a71e3ec074d71d6
BocCh1i1 <2607738829@qq.com>
2026-09-06 22:32:47 +0800

    补充登录逻辑(已完成)

--- src/UI/Login.java
+++ src/UI/Login.java
@@ -3,12 +3,12 @@ package UI;
 import domain.User;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Login {
     //登录主界面
     public void start() {
-        System.out.println("游戏的登录界面打开了");
 
         ArrayList<User> list = new ArrayList<>();
         while (true) {
@@ -36,6 +36,79 @@ public class Login {
     //登录操作
     public void login(ArrayList<User> list) {
         System.out.println("登录");
+        //### 2.2 登录功能：
+        //
+        //. 键盘录入用户名
+        //. 键盘录入密码
+        //. 键盘录入验证码
+        //. 登录最多重试三次，三次错误账号锁定
+        //
+        //             **验证要求：**
+        //
+        //用户名如果未注册提示：用户名未注册，请先注册
+        //
+        //用户被锁定提示：用户xxx已经锁定，请联系黑马程序员官方客服：XXX-XXXXX
+        //
+        //验证码错误提示：验证码输入错误，请重新输入，并生成一个新的验证码
+        //
+        //判断用户名和密码是否正确，有3次机会，满3次账户锁定。
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("请输入用户名：");
+        String username = sc.next();
+        if (!contains(list, username)) {
+            System.out.println("用户名未注册，请先注册");
+            return;
+        }
+
+        int index = findIndex(list, username);
+        User u = list.get(index);
+        if (!u.isStatus()) {
+            System.out.println("用户" + username + "已经锁定，请联系程序员官方客服：XXX-XXXXX");
+            return;
+        }
+
+        String rightPassword = u.getPassword();
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("请输入密码：");
+            String password = sc.next();
+
+
+            while (true) {
+                String rightCode = getCode();
+                System.out.println("验证码是：" + rightCode);
+
+                System.out.println("请输入验证码：");
+                String code = sc.next();
+
+
+                if (!code.equals(rightCode)) {
+                    System.out.println("验证码输入错误，请重新输入，并生成一个新的验证码");
+                } else {
+                    break;
+                }
+            }
+
+            if (password.equals(rightPassword)) {
+                System.out.println("登录成功");
+                return;
+            } else {
+                System.out.println("密码输入错误，请重新输入");
+                if (i == 2) {
+                    u.setStatus(false);
+                    System.out.println("用户" + username + "已经锁定，请联系程序员官方客服：XXX-XXXXX");
+                    return;
+                } else {
+                    System.out.println("还剩" + (2 - i) + "次机会");
+                }
+
+            }
+
+        }
+
+
     }
 
     //注册操作
@@ -103,6 +176,19 @@ public class Login {
         System.out.println("注册成功");
     }
 
+
+    //根据username找索引
+    public int findIndex(ArrayList<User> list, String username) {
+        for (int i = 0; i < list.size(); i++) {
+            User u = list.get(i);
+            if (u.getUsername().equals(username)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
     // 长度必须在minLen ~ maxLen之间
     public boolean checkLen(int minLen, int maxLen, String str) {
         return str.length() >= minLen && str.length() <= maxLen;
@@ -126,6 +212,7 @@ public class Login {
         }
         return new int[]{charCount, numCount, otherCount};
     }
+
     public boolean checkUsername(String username) {
         int[] counts = getCount(username);
         return counts[0] > 0 && counts[2] == 0;
@@ -147,6 +234,40 @@ public class Login {
         return false;
     }
 
+    public String getCode() {
+        //#### 2.3 验证码规则：
+        //长度为5
+        //由4位大写或者小写字母和1位数字组成，同一个字母可重复
+        //比如：aQa1K
+
+        //    验证码的字符集
+        ArrayList<Character> list = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            list.add((char) ('a' + i));
+            list.add((char) ('A' + i));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        Random r = new Random();
+        for (int i = 0; i < 4; i++) {
+            int index = r.nextInt(list.size());
+            char c = list.get(index);
+            sb.append(c);
+        }
+
+        sb.append(r.nextInt(10));
+
+        //	数字可以出现在任意位置
+        char[] arr = sb.toString().toCharArray();
+        int i = r.nextInt(arr.length);
+        char temp = arr[i];
+        arr[i] = arr[arr.length - 1];
+        arr[arr.length - 1] = temp;
+
+        String code = new String(arr);
+
+        return code;
+    }
 
 }
 
---
+++ src/domain/Test.java
@@ -0,0 +1,13 @@
+package domain;
+
+
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
+
+
